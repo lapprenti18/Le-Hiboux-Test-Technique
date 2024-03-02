b8b0251d2af31e9eaa25bf8834226630a6bc78f0
lapprenti18 <pokspid@gmail.com>
2024-03-02 11:24:10 +0100

    feat(server/src): change the db connection using .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
