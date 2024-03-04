47cf398e81c93b377fd82591856d2060f9f414b8
lapprenti18 <pokspid@gmail.com>
2024-03-04 11:48:54 +0100

    feat(server): create connection between nest and rabbitmq

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import configuration from './config/configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const user = configuration().rabbitmq.user;
+  const password = configuration().rabbitmq.password;
+  const host = configuration().rabbitmq.host;
+  const queueName = configuration().rabbitmq.queueName;
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://localhost:5672`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+  
+  app.startAllMicroservices();
+  app.listen(3000);
 }
 bootstrap();
