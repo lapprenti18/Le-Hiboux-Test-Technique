b1ac52f3ca6f74a2f7bd27ad39de36c6fece6c5d
lapprenti18 <pokspid@gmail.com>
2024-03-06 11:38:09 +0100

    feat(server/src/mail): create mail module and connection

---
+++ server/src/mail/mail.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import configuration from 'src/config/configuration';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        // service: 'QueueTest',
+        host: configuration().mail.host,
+        port: configuration().mail.port,
+        secure: false,
+        auth: {
+          user: configuration().mail.user,
+          pass: configuration().mail.password,
+        },
+      },
+      defaults: {
+        from: configuration().mail.user,
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
---
+++ server/src/mail/mail.service.ts
@@ -0,0 +1,23 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendCardCreation() {
+    try {
+      const mailContent = {
+        to: "pokspid@gmail.com",
+        subject: 'New card created',
+        context: {},
+      }
+
+      const emailSend = await this.mailerService.sendMail({
+        ...mailContent,
+      });
+    } catch (error) {
+      throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
