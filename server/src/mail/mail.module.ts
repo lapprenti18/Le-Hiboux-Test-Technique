f191c5ec98a86949098b02d8ff9ddc3cc277a34f
lapprenti18 <pokspid@gmail.com>
2024-03-06 12:15:15 +0100

    fix(server/src/mail): change the way to use the env variables

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -2,32 +2,35 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import configuration from 'src/config/configuration';
 import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigService
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        // service: 'QueueTest',
-        host: configuration().mail.host,
-        port: configuration().mail.port,
-        secure: false,
-        auth: {
-          user: configuration().mail.user,
-          pass: configuration().mail.password,
+    ConfigModule, 
+    MailerModule.forRootAsync({
+      inject: [ConfigService], 
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('mail.host'),
+          port: configService.get<number>('mail.port'),
+          secure: false,
+          auth: {
+            user: configService.get<string>('mail.user'),
+            pass: configService.get<string>('mail.password'),
+          },
         },
-      },
-      defaults: {
-        from: configuration().mail.user,
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
+        defaults: {
+          from: configService.get<string>('mail.user'),
         },
-      },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
     }),
   ],
   providers: [MailService],
