5c5e5eef770f5388bbd4cc33e64fd22c47f431bb
lapprenti18 <pokspid@gmail.com>
2024-03-06 14:51:23 +0100

    delete(server): remove useless rabbitMQ connection

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,28 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import configuration from './config/configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const user = configuration().rabbitmq.user;
-  const password = configuration().rabbitmq.password;
-  const host = configuration().rabbitmq.host;
-  const queueName = configuration().rabbitmq.queueName;
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`amqp://${user}:${password}@${host}`],
-      queue: queueName,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  app.startAllMicroservices();
   app.listen(3000);
 }
 bootstrap();
